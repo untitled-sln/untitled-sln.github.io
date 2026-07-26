86d70d5b59c5433c4ede747a5b7453e0c57b91f4
zlx-py <zlx876287@gmail.com>
2026-07-26 14:34:31 +0800

    fix

--- BlogProjBlazor/Program.cs
+++ BlogProjBlazor/Program.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Net.Http;
 using BlogProjBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
